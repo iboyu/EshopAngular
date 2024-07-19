472086fcb94b7e427f4380b162f77b07e3afbc5f
iboyu <yrl19951111@gmail.com>
2024-07-18 20:16:05 -0400

    Day 1 Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './Core/core.module';
+import { SharedModule } from './Shared/shared.module';
+import { ProductListComponent } from './Public/product-list.component';
+import { ProductDetailsComponent } from './Public/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    SharedModule,
+    NgbModule
   ],
   providers: [
     provideClientHydration()
