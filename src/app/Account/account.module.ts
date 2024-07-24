8af6efa53da01873142154d2fca3f4ba385febc1
iboyu <yrl19951111@gmail.com>
2024-07-23 21:42:02 -0400

    Reactive Forms finished

--- src/app/Account/account.module.ts
+++ src/app/Account/account.module.ts
@@ -4,11 +4,16 @@ import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { LoginComponent } from './login.component';
 import { RegisterComponent } from './register.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, AccountRoutingModule, FormsModule],
+  imports: [
+    CommonModule,
+    AccountRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   exports: [LoginComponent, RegisterComponent],
 })
 export class AccountModule {}
--- src/app/Core/Services/account.service.ts
+++ src/app/Core/Services/account.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Register } from '../../Shared/Models/Register';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AccountService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  Register(registerData: Register): Observable<boolean> {
+    let headers = new HttpHeaders();
+    headers = headers.set('xxxxx-key', 'cxawasdfafs');
+    return this.http.post<boolean>(
+      'https://xdadasdasds/api/Account/Login',
+      registerData,
+      {
+        headers: { 'xxxxx-key': environment.subscriptionKey },
+      }
+    );
+  }
 }
